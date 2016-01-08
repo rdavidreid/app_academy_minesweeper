ee1affff172f99f077a71dcf54fd004fec07896b
App Academy Student <student@appacademy.io>
2016-01-08 15:54:46 -0800

    Add render to board

--- board.rb
+++ board.rb
@@ -7,7 +7,8 @@ class Board
   def initialize(size)
     @size = size
     @grid = Array.new(size){Array.new(size)}
-    @total_bombs = (size ** 2) / 3
+    @total_bombs = 10#(size ** 2) / 3
+    populate
   end
 
   def populate
@@ -19,6 +20,25 @@ class Board
         grid[row][col] = Tile.new(self, pos, bomb_status)
       end
     end
+  end
+
+  def render
+    grid.each_with_index do |row, i|
+      puts
+      row.each_with_index do |col, j|
+        tile = grid[i][j]
+        if tile.flagged
+          print "F "
+        elsif tile.value
+          print "#{tile.value} "
+        elsif tile.revealed
+          print "_ "
+        else
+          print "* "
+        end
+      end
+    end
+    puts
   end
 
   def [](pos)
