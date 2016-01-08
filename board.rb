8f7651c361c2348f5fc396317842bfd738021d86
App Academy Student <student@appacademy.io>
2016-01-08 13:29:13 -0800

    add grid syntax

--- board.rb
+++ board.rb
@@ -1,7 +1,24 @@
 class Board
+
+  attr_reader :grid
+
   def initialize(size)
     @size = size
     @grid = Array.new(size){Array.new(size)}
     end
+
+  def populate
+
+  end
+
+  def [](pos)
+    x,y = pos[0],pos[1]
+    grid[x][y]
+  end
+
+  def []=(pos,value)
+    x,y = pos[0],pos[1]
+    grid[x][y] = value
+  end
 
 end
