72e29dc0a92858e7d70997facb23d78480944674
App Academy Student <student@appacademy.io>
2016-01-08 14:11:39 -0800

    add bomb_shuffle and populate

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+require_relative 'tile.rb'
+
 class Board
 
   attr_reader :grid
@@ -5,10 +7,18 @@ class Board
   def initialize(size)
     @size = size
     @grid = Array.new(size){Array.new(size)}
-    end
+    @total_bombs = (size ** 2) / 3
+  end
 
   def populate
-
+    super_duper_bomb_array = bomb_shuffle
+    grid.each_with_index do |_,row|
+      grid[row].each_with_index do |_,col|
+        bomb_status = super_duper_bomb_array.shift
+        pos = [row,col]
+        grid[row][col] = Tile.new(self, pos, bomb_status)
+      end
+    end
   end
 
   def [](pos)
@@ -19,6 +29,12 @@ class Board
   def []=(pos,value)
     x,y = pos[0],pos[1]
     grid[x][y] = value
+  end
+
+  def bomb_shuffle
+    result = Array.new(@size ** 2, false)
+    @total_bombs.times {|i| result[i] = true}
+    result.shuffle
   end
 
 end
